cb14a2141d014f1ba7666e27019c39097339cd79
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-03-16 22:08:29 -0300

    header e routes

---
+++ src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default (props) => (
+  <div>
+    <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+    <h2>Nossa História</h2>
+    <p>Nossa chácara começou</p>
+    <h2>O melhor ambiente para família</h2>
+    <p>É um paraíso para férias em família</p>
+    <h2>Descanse!</h2>
+    <p>Piscina, mesa de sinuca, churrasqueira</p>
+  </div>
+);
